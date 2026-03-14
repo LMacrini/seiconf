2b99fcfad1d795b7bbe648c2486a1663fb61f601
Seija <seijamail@duck.com>
2026-03-14 19:03:20 -0400

    xdg user dirs as a separate module

--- modules/nixosModules/features/hjem.nix
+++ modules/nixosModules/features/hjem.nix
@@ -4,41 +4,118 @@
   self,
   ...
 }: {
-  flake.aspects.hjem.module = {
+  # probably not a necessary thing, will be doing it anyway :3
+  flake.hjemModules.userDirs = {
     config,
     pkgs,
     ...
   }: let
-    home = config.hjem.users.lioma.directory;
-    dirs = {
-      DOCUMENTS = "${home}/Documents";
-      DOWNLOAD = "${home}/Downloads";
-      GAMES = "${home}/Games";
-      MUSIC = "${home}/Music";
-      PICTURES = "${home}/Pictures";
-      PROJECTS = "${home}/Projects";
-      PUBLICSHARE = "${home}/Public";
-      VIDEOS = "${home}/Videos";
+    inherit (lib) mkOption types;
+    dir = with types; coercedTo path toString str;
+
+    cfg = config.xdg.userDirs;
+  in {
+    options = {
+      xdg.userDirs = {
+        enable = lib.mkEnableOption "xdg user dirs";
+        createDirectories = lib.mkEnableOption "automatic creation of xdg user dirs" // {default = true;};
+
+        package = lib.mkPackageOption pkgs "xdg-user-dirs" {nullable = true;};
+
+        desktop = mkOption {
+          type = types.nullOr dir;
+          default = null; # the desktop directory sucks
+        };
+
+        documents = mkOption {
+          type = types.nullOr dir;
+          default = "${config.directory}/Documents";
+        };
+
+        download = mkOption {
+          type = types.nullOr dir;
+          default = "${config.directory}/Downloads";
+        };
+
+        music = mkOption {
+          type = types.nullOr dir;
+          default = "${config.directory}/Music";
+        };
+
+        pictures = mkOption {
+          type = types.nullOr dir;
+          default = "${config.directory}/Pictures";
+        };
+
+        publicShare = mkOption {
+          type = types.nullOr dir;
+          default = "${config.directory}/Public";
+        };
+
+        videos = mkOption {
+          type = types.nullOr dir;
+          default = "${config.directory}/Videos";
+        };
+
+        extraConfig = mkOption {
+          type = types.attrsOf dir;
+          default = {};
+        };
+
+        setSessionVariables = lib.mkEnableOption "setting xdg user dir session variables";
+      };
     };
 
-    bindings = lib.mapAttrs' (k: lib.nameValuePair "XDG_${k}_DIR") dirs;
-  in {
+    config = let
+      directories =
+        (lib.filterAttrs (n: v: !isNull v) {
+          DESKTOP = cfg.desktop;
+          DOCUMENTS = cfg.documents;
+          DOWNLOAD = cfg.download;
+          MUSIC = cfg.music;
+          PICTURES = cfg.pictures;
+          PUBLICSHARE = cfg.publicShare;
+          VIDEOS = cfg.videos;
+        })
+        // cfg.extraConfig;
+
+      bindings = lib.mapAttrs' (k: lib.nameValuePair "XDG_${k}_DIR") directories;
+    in
+      lib.mkIf cfg.enable {
+        xdg.config.files = {
+          "user-dirs.dirs" = {
+            generator = lib.generators.toKeyValue {};
+            value = builtins.mapAttrs (_: value: ''"${value}"'') bindings;
+          };
+
+          # disables xdg-user-dirs-update, which tries to modify $XDG_CONFIG_HOME/user-dirs.dirs
+          "user-dirs.conf".text = "enabled=False";
+        };
+
+        packages = lib.mkIf (cfg.package != null) [cfg.package];
+        environment.sessionVariables = lib.mkIf (cfg.setSessionVariables) bindings;
+
+        systemd.services.createXdgUserDirectories = lib.mkIf (cfg.createDirectories) {
+          description = "creates xdg user directories";
+          wantedBy = ["default.target"];
+
+          script = let
+            directoriesList = lib.attrValues directories;
+            mkdir = dir: ''[[ -L "${dir}" ]] || mkdir -p "${dir}"'';
+          in
+            lib.concatMapStringsSep "\n" mkdir directoriesList;
+
+          serviceConfig = {
+            Type = "oneshot";
+          };
+        };
+      };
+  };
+
+  flake.aspects.hjem.module = {config, ...}: {
     imports = [
       inputs.hjem.nixosModules.default
     ];
-
-    systemd.services.createXdgUserDirectories = let
-      directoriesList = lib.attrValues dirs;
-      mkdir = dir: "[[ -L ${dir} ]] || mkdir -p ${dir}";
-    in {
-      after = [
-        "hjem-activate@lioma.service"
-      ];
-      partOf = ["hjem.target"];
-      wantedBy = ["hjem.target"];
-
-      script = lib.concatMapStringsSep "\n" mkdir directoriesList;
-    };
 
     hjem = let
       mod = {
@@ -48,6 +125,7 @@
             default = "graphical-session.target";
           };
 
+          # TODO: make this system wide
           configDirectory = mkOption {
             type = types.str;
           };
@@ -56,6 +134,7 @@
     in {
       extraModules = [
         inputs.hjem-rum.hjemModules.default
+        self.hjemModules.userDirs
         mod
       ];
 
@@ -74,30 +153,25 @@
           source = config.hjem.users.lioma.environment.loadEnv;
         };
 
-        packages = [
-          pkgs.xdg-user-dirs
-        ];
-
-        environment.sessionVariables =
-          bindings
-          // {
-            NH_FLAKE = cfg.configDirectory;
-          };
+        environment.sessionVariables = {
+          NH_FLAKE = cfg.configDirectory;
+        };
 
         xdg.config.files = {
-          "user-dirs.dirs" = {
-            generator = lib.generators.toKeyValue {};
-            value = builtins.mapAttrs (_: value: ''"${value}"'') bindings;
-          };
-
-          "user-dirs.conf".text = "enabled=False";
-
           "fish/conf.d/hjem-environment.fish" = lib.mkIf (cfg.environment.sessionVariables != {}) {
             text =
               lib.concatMapAttrsStringSep "\n" (
                 name: value: "set -gx ${lib.escapeShellArg name} ${lib.escapeShellArg (toString value)}"
               )
               cfg.environment.sessionVariables;
+          };
+        };
+
+        xdg.userDirs = {
+          enable = true;
+          extraConfig = {
+            GAMES = "${cfg.directory}/Games";
+            PROJECTS = "${cfg.directory}/Projects";
           };
         };
       };
