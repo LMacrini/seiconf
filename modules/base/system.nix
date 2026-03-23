024819392ebea1252cb13dfd3648ac73354ec17d
Seija <seijamail@duck.com>
2026-03-23 19:19:24 -0400

    nh cleaning

--- modules/base/system.nix
+++ modules/base/system.nix
@@ -1,10 +1,19 @@
-{
+{lib, ...}: {
   flake.nixosModules.base = {
     system.nixos = {
       distroId = "seios";
       distroName = "SeiOS";
       vendorId = "seios";
       vendorName = "SeiOS";
+    };
+
+    programs.nh = {
+      enable = true;
+      clean = {
+        enable = true;
+        dates = "daily";
+        extraArgs = lib.mkDefault "--keep 5 --keep-since 14d";
+      };
     };
   };
 }
