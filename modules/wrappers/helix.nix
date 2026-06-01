f66544362ac4f5c4f14af362735f7b4fd2fcefe9
Seija <seijamail@duck.com>
2026-06-01 16:16:39 -0400

    use evil helix for indent guides

--- modules/wrappers/helix.nix
+++ modules/wrappers/helix.nix
@@ -1,8 +1,10 @@
 {
   flake.wrappers.helix =
-    { wlib, ... }:
+    { pkgs, wlib, ... }:
     {
       imports = [ wlib.wrapperModules.helix ];
+
+      package = pkgs.evil-helix;
 
       settings = {
         theme = "catppuccin_macchiato";
@@ -10,9 +12,15 @@
         editor = {
           auto-pairs = false;
           color-modes = true;
+          evil = false;
           line-number = "relative";
           mouse = false;
           trim-trailing-whitespace = true;
+
+          indent-guides = {
+            render = true;
+            rainbow-option = "dim";
+          };
 
           cursor-shape = {
             insert = "bar";
